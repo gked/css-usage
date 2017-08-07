bc1cfa56ac60105e1bd46d88fb3f69742ee0feab
Grisha Lyukshin <glyuk@microsoft.com>
2017-08-07 15:01:46 -0700

    files updated by grunt

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1541,6 +1541,24 @@ void function() { try {
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
 /* 
+    RECIPE: <NAME OF RECIPE>
+    -------------------------------------------------------------
+    Author: Grisha Lyukshin
+    Description: usage of elementFromPoint in script tags
+*/
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function getElementFromPointUsage( element, results) {
+
+    	if(element.nodeName === "SCRIPT"){
+    		if(element.innerText.indexOf("elementFromPoint") !== -1){
+    			results["use"] = results["use"] || {count: 0};
+    			results["use"].count++;
+    		}
+    	}
+        return results;
+    });
+}();
+/* 
     RECIPE: z-index on static flex items
     -------------------------------------------------------------
     Author: Francois Remy
