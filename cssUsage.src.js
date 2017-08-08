f2280a16883da3ca355a676882ddf422fa94659d
Grisha Lyukshin <glyuk@microsoft.com>
2017-08-07 18:50:27 -0700

    adding new recipe for elementfrompoint api

--- cssUsage.src.js
+++ cssUsage.src.js
@@ -1541,16 +1541,16 @@ void function() { try {
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
 /* 
-    RECIPE: Request Payment
+    RECIPE: elementFromPoint
     -------------------------------------------------------------
-    Author: Mustapha Jaber
-    Description: Find use of RequestPayment in script.
+    Author: Grisha Lyukshin
+    Description: Find use of elementFromPoint in script.
 */
 
 void function() {
-    window.CSSUsage.StyleWalker.recipesToRun.push( function paymentRequest(/*HTML DOM Element*/ element, results) {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function elementFromPointUsage(/*HTML DOM Element*/ element, results) {
         var nodeName = element.nodeName;
-        var script = "RequestPayment"
+        var script = ".elementFromPoint"
         if (nodeName == "SCRIPT")
         {
             results[nodeName] = results[nodeName] || { count: 0, };
@@ -1562,12 +1562,16 @@ void function() {
             else if (element.src !== undefined && element.src != "")
             {
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", element.src, false);
+                xhr.open("GET", element.src, true);
                 //xhr.setRequestHeader("Content-type", "text/javascript");
                 xhr.send();
-                if (xhr.status === 200 && xhr.responseText.indexOf(script) != -1)
-                {
-                    results[nodeName].count++;
+                xhr.onreadystatechange = function() {
+                    if(this.readyState == this.HEADERS_RECEIVED ) {
+                        if (xhr.status === 200 && xhr.responseText.indexOf(script) != -1)
+                        {
+                            results[nodeName].count++;
+                        }
+                    }
                 }
             }
         }
