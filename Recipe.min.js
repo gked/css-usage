06e5bb20ec0b4b023fd65eea5ad243482c9dd153
Grisha Lyukshin <glyuk@microsoft.com>
2017-08-08 14:43:35 -0700

    creating caretRangeFromPoint recipe

--- Recipe.min.js
+++ Recipe.min.js
@@ -1541,6 +1541,44 @@ void function() { try {
 } catch (ex) { /* do something maybe */ throw ex; } }();
 
 /* 
+    RECIPE: caretRangeFromPoint
+    -------------------------------------------------------------
+    Author: Grisha Lyukshin
+    Description: Find use of caretRangeFromPoint in script.
+*/
+
+void function() {
+    window.CSSUsage.StyleWalker.recipesToRun.push( function caretRangeFromPoint(/*HTML DOM Element*/ element, results) {
+        var nodeName = element.nodeName;
+        var script = ".caretRangeFromPoint"
+        if (nodeName == "SCRIPT")
+        {
+            results[nodeName] = results[nodeName] || { count: 0, };
+            // if inline script. ensure that it's not our recipe script and look for string of interest
+            if (element.text !== undefined && element.text.indexOf(script) != -1)
+            {
+                results[nodeName].count++;
+            }
+            else if (element.src !== undefined && element.src != "")
+            {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", element.src, true);
+                //xhr.setRequestHeader("Content-type", "text/javascript");
+                xhr.send();
+                xhr.onreadystatechange = function() {
+                    if(this.readyState == this.HEADERS_RECEIVED ) {
+                        if (xhr.status === 200 && xhr.responseText.indexOf(script) != -1)
+                        {
+                            results[nodeName].count++;
+                        }
+                    }
+                }
+            }
+        }
+        return results;
+    });
+}();
+/* 
     RECIPE: elementFromPoint
     -------------------------------------------------------------
     Author: Grisha Lyukshin
